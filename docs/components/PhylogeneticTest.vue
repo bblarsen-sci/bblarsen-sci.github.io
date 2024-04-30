0ffca5c6d4115b76213eee9b70124a67255847e6
Brendan Larsen <brendanlarsen@Brendans-Air.lan>
2024-04-29 17:52:09 -0700

    update d3 heatmap

--- docs/components/PhylogeneticTest.vue
+++ docs/components/PhylogeneticTest.vue
@@ -1,75 +1,81 @@
----
-# https://vitepress.dev/reference/default-theme-home-page
-layout: page
----
-
-
-
-<html>
-<body>
-<div class=" m-4 text-xl font-semibold">Effect of mutations on cell entry of RBP in CHO-bEFNB2 cells</div>
-<div class="flex flex-col justify-left items-left">
-    <div class="m-6 text-xs">
-        <label for="wildtypeSelect" class="mr-2">Select Wildtype Amino Acid:</label>
-        <select id="wildtypeSelect" class="px-2 py-2 rounded-md ring-2 ring-slate-800 focus:outline-none focus:ring-2 focus:ring-blue-500">
+<template>
+    <div>
+      <div class="m-4 text-xl font-semibold">Effect of mutations on cell entry of RBP in CHO-bEFNB2 cells</div>
+      <div class="flex flex-col justify-left items-left">
+        <div class="m-6 text-xs">
+          <label for="csvUpload" class="mr-2">Upload CSV:</label>
+          <input type="file" id="csvUpload" @change="handleFileUpload" accept=".csv" class="px-2 py-2 rounded-md ring-2 ring-slate-800 focus:outline-none focus:ring-2 focus:ring-blue-500" />
+        </div>
+        <div class="m-6 text-xs">
+          <label for="wildtypeSelect" class="mr-2">Select Wildtype Amino Acid:</label>
+          <select id="wildtypeSelect" v-model="selectedAminoAcid" class="px-2 py-2 rounded-md ring-2 ring-slate-800 focus:outline-none focus:ring-2 focus:ring-blue-500">
             <option value="">All</option>
-            <option value="R">R</option>
-            <option value="K">K</option>
-            <option value="H">H</option>
-            <option value="D">D</option>
-            <option value="E">E</option>
-            <option value="Q">Q</option>
-            <option value="N">N</option>
-            <option value="S">S</option>
-            <option value="T">T</option>
-            <option value="Y">Y</option>
-            <option value="W">W</option>
-            <option value="F">F</option>
-            <option value="A">A</option>
-            <option value="I">I</option>
-            <option value="L">L</option>
-            <option value="M">M</option>
-            <option value="V">V</option>
-            <option value="G">G</option>
-            <option value="P">P</option>
-            <option value="C">C</option>
-        </select>
+            <option v-for="aa in aminoAcids" :key="aa" :value="aa">{{ aa }}</option>
+          </select>
+        </div>
+        <div ref="svgContainer" id="svgContainer"></div>
+      </div>
+      <div ref="tooltip" id="tooltip" class="tooltip"></div>
     </div>
-    <div ref="svgContainer" id="svgContainer"></div>
-</div>
-<div ref="tooltip" id="tooltip" class="tooltip"></div>
-</body>
-</html>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted, watch } from 'vue';
+  import * as d3 from 'd3';
+  
+  const svgContainer = ref(null);
+  const tooltip = ref(null);
+  const selectedAminoAcid = ref('');
+  const parsedData = ref([]);
+  const filteredData = ref([]);
+  
+  const aminoAcids = [
+    "R", "K", "H", "D", "E", "Q", "N", "S", "T", "Y",
+    "W", "F", "A", "I", "L", "M", "V", "G", "P", "C"
+  ];
+  
+  onMounted(async () => {
+    const response = await fetch('https://raw.githubusercontent.com/dms-vep/Nipah_Malaysia_RBP_DMS/master/results/filtered_data/public_filtered/RBP_mutation_effects_cell_entry_CHO-bEFNB2.csv');
+    const data = await response.text();
+    parsedData.value = d3.csvParse(data);
+    filterData();
+    updateHeatmap();
+  });
+  
+  function handleFileUpload(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+  
+    reader.onload = () => {
+      parsedData.value = d3.csvParse(reader.result);
+      filterData();
+      updateHeatmap();
+    };
+  
+    reader.readAsText(file);
+  }
+  
+  function filterData() {
+    if (selectedAminoAcid.value) {
+      filteredData.value = parsedData.value.filter(d => +d.site >= 71 && +d.site <= 602 && d.wildtype === selectedAminoAcid.value);
+    } else {
+      filteredData.value = parsedData.value.filter(d => +d.site >= 71 && +d.site <= 602);
+    }
+  }
+  
+  watch(selectedAminoAcid, () => {
+    filterData();
+    updateHeatmap();
+  });
 
-<script setup>
-import { ref, computed, onMounted } from 'vue';
-import * as d3 from 'd3';
-
-const svgContainer = ref(null);
-const tooltip = ref(null);
-
-const amino_acids = [
-  "R", "K", "H", "D", "E", "Q", "N", "S", "T", "Y",
-  "W", "F", "A", "I", "L", "M", "V", "G", "P", "C"
-];
-
-onMounted(async () => {
-const response = await fetch('https://raw.githubusercontent.com/dms-vep/Nipah_Malaysia_RBP_DMS/master/results/filtered_data/public_filtered/RBP_mutation_effects_cell_entry_CHO-bEFNB2.csv');
-const data = await response.text();
-const parsedData = d3.csvParse(data);
-
-let filteredData = parsedData.filter(d => +d.site >= 71 && +d.site <= 602);
-
-
-function updateHeatmap() {
+  function updateHeatmap() {
   const svg = d3.select('#svgContainer');
   const height = 300;
   const margin = { top: 20, right: 20, bottom: 40, left: 60 };
   const innerHeight = height - margin.top - margin.bottom;
-  
 
-  const sites = [...new Set(filteredData.map(d => +d.site))];
-  const squareSize = Math.min(innerHeight / amino_acids.length, 20);
+  const sites = [...new Set(filteredData.value.map(d => +d.site))];
+  const squareSize = Math.min(innerHeight / aminoAcids.length, 20);
   const innerWidth = squareSize * sites.length;
   const width = innerWidth + margin.left + margin.right;
   
@@ -187,8 +193,4 @@ d3.select('#wildtypeSelect').on('change', function() {
   updateHeatmap();
 });
 
-
-
-updateHeatmap();
-});
 </script>
