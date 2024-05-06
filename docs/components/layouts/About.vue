fabad52f46e3ddda8575a5dffe03c1630a293b4e
Brendan Larsen <brendanlarsen@Brendans-Air.lan>
2024-05-05 21:55:30 -0700

    update

--- docs/components/layouts/About.vue
+++ docs/components/layouts/About.vue
@@ -1,24 +1,22 @@
 <template>
-    <div class="dark:bg-slate-900 min-h-screen text-slate-800 dark:text-slate-400">
-        <div class="">
-            <div class="flex flex-col justify-center h-80 lg:h-96">
-                <div class="text-left text-4xl 2xl:text-6xl font-semibold px-4 lg:px-10 xl:px-24 2xl:px-30">
-                    <p>About</p>
-                </div>
+    <div class="mx-auto max-w-screen-md relative ">
+        <div class="h-36 flex items-center mx-2">
+            <div class="text-left">
+                <div class="font-semibold text-2xl">About</div>
             </div>
-            <div class="px-4 lg:px-10 xl:px-24 2xl:px-30 font-light text-lg lg:text-xl">
-                <img src="/images/pallid.jpg" class="float-right ml-6 mb-6 w-48 lg:w-72 2xl:w-80 object-cover shadow-lg shadow-slate-400"></img>
-                <p class="text-left">
-                    I am broadly interested in how viruses evolve in wild mammals, in particular bats and rodents. I am currently a postdoctoral fellow at the <a class="text-blue-400 hover:text-blue-600" href="https://www.fredhutch.org/en.html">Fred Hutch Cancer Center</a>, in Seattle, Washington, where I study Nipah virus in the <a class="text-blue-400 hover:text-blue-600" href="https://research.fredhutch.org/bloom/en.html">Bloom lab</a>.
-                </p>
-                <p class="text-left">
-                    I received my PhD in 2021 from the University of Arizona in Tucson, Arizona, in the <a class="text-blue-400 hover:text-blue-600" href="https://eeb.arizona.edu">Department of Ecology and Evolutionary Biology</a>. I worked with my PhD advisor, Dr. Michael Worobey, to study the evolution of paramyxoviruses in wild mammals and the spread of SARS-CoV-2 during the early stages of the pandemic.
-                </p>
-                <img src="/images/1232541671.jpg" class=""></img>
-                <p class="text-left">
-                    Prior to my PhD, I worked as a research technician in the lab of <a class="text-blue-400 hover:text-blue-600" href="https://mullinslab.microbiol.washington.edu/people/faculty/1460-James-Mullins">Dr. James Mullins</a> at the University of Washington, in Seattle, Washington. I worked on projects that involved sequencing HIV genomes from breakthrough infections in vaccine trials and early infection.
-                </p>
-            </div>
+        </div>
+        <div class="container inline-block mx-2">
+            <img src="/images/pallid.jpg" class="float-right ml-6 mb-4 max-w-48 object-cover aspect-square"></img>
+            <p class="text-left mb-4">
+                I am broadly interested in how viruses evolve in wild mammals, in particular bats and rodents. I am currently a postdoctoral fellow at the <a class="text-blue-400 hover:text-blue-600" href="https://www.fredhutch.org/en.html">Fred Hutch Cancer Center</a>, in Seattle, Washington, where I study Nipah virus in the <a class="text-blue-400 hover:text-blue-600" href="https://research.fredhutch.org/bloom/en.html">Bloom lab</a>.
+            </p>
+            <p class="text-left mb-4">
+                I received my PhD in 2021 from the University of Arizona in Tucson, Arizona, in the <a class="text-blue-400 hover:text-blue-600" href="https://eeb.arizona.edu">Department of Ecology and Evolutionary Biology</a>. I worked with my PhD advisor, Dr. Michael Worobey, to study the evolution of paramyxoviruses in wild mammals and the spread of SARS-CoV-2 during the early stages of the pandemic.
+            </p>
+            <img src="/images/1232541671.jpg" class="float-left max-h-60 mr-6 mt-6"></img>
+            <p class="text-left">
+                Prior to my PhD, I worked as a research technician in the lab of <a class="text-blue-400 hover:text-blue-600" href="https://mullinslab.microbiol.washington.edu/people/faculty/1460-James-Mullins">Dr. James Mullins</a> at the University of Washington, in Seattle, Washington. I worked on projects that involved sequencing HIV genomes from breakthrough infections in vaccine trials and early infection.
+            </p>
         </div>
     </div>
 </template>
--- docs/components/layouts/Publications.vue
+++ docs/components/layouts/Publications.vue
@@ -33,23 +33,28 @@ export default {
 
 
 <template>
-  <div class="text-slate-800 dark:text-slate-400 bg-white dark:bg-slate-900">
-    <div class="flex flex-col justify-center h-80 md:h-96 bg-layeredpeaks">
-      <div class="text-left text-4xl 2xl:text-6xl tracking-tight leading-8 font-bold px-8">
-        <p>Publications</p>
-      </div>
+  <div class="mx-0 lg:mx-4">
+    <div class="h-36 flex items-center mx-2">
+        <div class="text-left">
+            <div class="font-semibold text-2xl">Publications</div>
+        </div>
     </div>
-    <div class="pb-10">
+    <div class="flex items-center mx-auto gap-2 justify-between pb-4 text-sm md:text-lg tracking-wider uppercase opacity-70 text-center">
+      <a class="inline-flex flex-1 justify-center hover:text-sky-500" href="#section-1">Preprints</a>
+      <a class="inline-flex flex-1 justify-center hover:text-sky-500" href="#section-2">First author </a>
+      <a class="inline-flex flex-1 justify-center hover:text-sky-500" href="#section-3">Coauthored</a>
+    </div>
+    <div class="pb-10 mx-2">
       <div class="h-1 bg-slate-400"></div>
-      <h2 class="text-2xl 2xl:text-4xl leading-8 tracking-tight font-bold py-10 px-8">Preprints</h2>
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-8">
+      <h2 class="mx-2 text-2xl leading-8 tracking-tight font-bold py-10 ">Preprints</h2>
+      <div id="section-1" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-evenly gap-20 mx-20">
         <div v-for="paper in preprints" :key="paper.title" class="card">
           <a :href="paper.link" class="block h-full">
-            <div class="h-full flex flex-col justify-between space-y-2 p-6 bg-white dark:bg-slate-700 rounded-xl shadow-md shadow-slate-500">
+            <div class="h-full flex flex-col justify-evenly space-y-2 p-6 bg-white dark:bg-slate-700 rounded-xl shadow-md shadow-slate-500">
               <div class="space-y-4">
-                <div class="text-2xl leading-8 font-semibold tracking-tight">{{ paper.title }}</div>
+                <div class="text-xl leading-8 font-semibold tracking-tight">{{ paper.title }}</div>
                 <div class="flex justify-center">
-                  <img :src="paper.image" class="object-contain h-56 w-full">
+                  <img :src="paper.image" class="object-contain aspect-square max-h-36 w-full">
                 </div>
                 <div class="text-center">{{ paper.journal }}, {{ paper.year }}</div>
                 <p class="line-clamp-3 keywords">{{ paper.abstract }}</p>
@@ -61,13 +66,13 @@ export default {
     </div>
     <div class="pb-10">
       <div class="h-1 bg-slate-400"></div>
-      <h2 class="text-2xl 2xl:text-4xl leading-8 tracking-tight font-bold py-10 px-8">First author publications</h2>
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-8">
+      <h2 class="text-2xl leading-8 tracking-tight font-bold py-10 px-8">First author publications</h2>
+      <div id="section-2" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-8">
         <div v-for="paper in publications" :key="paper.title" class="card">
           <a :href="paper.link" class="block h-full">
             <div class="h-full flex flex-col justify-between space-y-2 p-6 bg-white dark:bg-slate-700 rounded-xl shadow-md shadow-slate-500">
               <div class="space-y-4">
-                <div class="text-2xl leading-8 font-semibold tracking-tight">{{ paper.title }}</div>
+                <div class="text-xl leading-8 font-semibold tracking-tight">{{ paper.title }}</div>
                 <div class="flex justify-center">
                   <img :src="paper.image" class="object-contain h-56 w-full">
                 </div>
@@ -81,13 +86,13 @@ export default {
     </div>
     <div class="pb-10">
       <div class="h-1 bg-slate-400"></div>
-      <h2 class="text-2xl 2xl:text-4xl leading-8 tracking-tight font-bold py-10 px-8">Co-authored Publications</h2>
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-8">
+      <h2 class="text-2xl leading-8 tracking-tight font-bold py-10 px-8">Co-authored Publications</h2>
+      <div id="section-3" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-8">
         <div v-for="paper in coauthor" :key="paper.title" class="card">
           <a :href="paper.link" class="block h-full">
             <div class="h-full flex flex-col justify-between space-y-2 p-6 bg-white dark:bg-slate-700 rounded-xl shadow-md shadow-slate-500">
               <div class="space-y-4">
-                <div class="text-2xl leading-8 font-semibold tracking-tight">{{ paper.title }}</div>
+                <div class="text-xl leading-8 font-semibold tracking-tight">{{ paper.title }}</div>
                 <div class="text-center">{{ paper.journal }}, {{ paper.year }}</div>
                 <p class="line-clamp-3 keywords">{{ paper.abstract }}</p>
               </div>
