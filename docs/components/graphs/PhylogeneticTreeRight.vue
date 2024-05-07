d8afcabf93c05b381ef3a64dc76eabf5a0536de6
Brendan Larsen <blarsen@bsbrendmacbloom.fhcrc.org>
2024-05-07 13:09:11 -0700

    make tree

--- docs/components/graphs/PhylogeneticTreeRight.vue
+++ docs/components/graphs/PhylogeneticTreeRight.vue
@@ -31,80 +31,117 @@ function parseNewick(a) {
       case ":":
         break;
       default:
-      var h = s[t - 1];
-if (h === ")" || h === "(" || h === ",") {
-  const nameAndCountry = n.split(/\[|\]/);
-  r.name = nameAndCountry[0];
-  r.country = nameAndCountry[1]; // Extract the country information
-} else if (h === ":") {
-  r.length = parseFloat(n);
-}
+        var h = s[t - 1];
+        if (h === ")" || h === "(" || h === ",") {
+          const nameAndCountry = n.split(/\[|\]/);
+          r.name = nameAndCountry[0];
+          r.country = nameAndCountry[1]; // Extract the country information
+        } else if (h === ":") {
+          r.branchLength = parseFloat(n);
+        }
     }
   }
   return r;
-};
-
-function maxLength(d) {
-  return d.data.length + (d.children ? d3.max(d.children, maxLength) : 0);
 }
+
+function projection(d) {
+        // reversed projection - horizontal tree instead of vertical
+        return [parseInt(d.y), parseInt(d.x)];
+}
+
+function diagonal(diagonalPath, i) {
+        // draw the hooked paths between nodes
+        var source = diagonalPath.source,
+            target = diagonalPath.target,
+            pathData = [source, {x: target.x, y: source.y}, target];
+            pathData = pathData.map(projection);
+
+        return "M" + pathData[0] + ' ' + pathData[1] + " " + pathData[2];
+    }
+
+
+function scaleBranchLengths(nodes, w) {
+    function visitPreOrder(root, callback) {
+      callback(root)
+      if (root.children) {
+        for (var i = root.children.length - 1; i >= 0; i--){
+          visitPreOrder(root.children[i], callback)
+        };
+      }
+    }
+
+    visitPreOrder(nodes[0], function(node) {
+      if (node.branchLength < 0) node.branchLength = -1 * node.branchLength;
+      node.rootDist = (node.parent ? node.parent.rootDist : 0) + (node.branchLength || 0)
+    });
+
+    var rootDists = nodes.map(function(n) { 
+      return n.rootDist; 
+    });
+
+    var yscale = d3.scaleLinear()
+      .domain([0, d3.max(rootDists)])
+      .range([0, w]);
+
+    visitPreOrder(nodes[0], function(node) {
+      node.y = parseInt(yscale(node.rootDist));
+    });
+    return yscale
+}
+
+
 function drawChart(data) {
-  const height = 600;
-  const width = 600;
-  const margin = { top: 20, right: 20, bottom: 20, left: 20 };
-  const outerRadius = width / 2;
-  const innerRadius = outerRadius - 120;
-  
-  const tree = d3.tree()
-    .size([height - margin.top - margin.bottom, width - margin.left - margin.right])
-
-  const root = d3.hierarchy(data, d => d.branchset)
-      //.sum(d => d.branchset )
-      .sort((a, b) => b.height - a.height || d3.ascending(a.id, b.id));
-        
-  const cluster = d3.cluster()
-    .size([width-margin.left - margin.right, height-margin.top - margin.bottom])
-  
+  console.log('data', data);
+  var margin = { top: 20, right: 10, bottom: 20, left: 10 };
+  var width = 500 - margin.left - margin.right;
+  var height = 600 - margin.top - margin.bottom;
 
 
-  // Make svg elements
-  const svg = d3.select(svgContainer.value);
-  svg.selectAll("*").remove();
+  var root = d3.hierarchy(data, d => d.branchset)
+    //.sort((a, b) => b.height - a.height || d3.ascending(a.id, b.id));
+
+
+  var tree = d3.cluster()
+    .size([height, width])
+    .separation(function separation(a, b) {
+      return a.parent == b.parent ? 1 : 1;
+    })
+    
   
-  const svgElement = svg.append('svg')
-    .attr('width', width)
-    .attr('height', height)
-    .attr('viewBox', `0 0 ${width} ${height}`)
-    .append('g')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
-  
-  // Initialize SVG properly
-  const g = svgElement.append("g")
-    .attr("transform", `translate(0, ${root.dy})`);
-  
-  
-    var link = g.selectAll(".link")
-    .data(tree(root).links())
+  var nodes = tree(root);
+  console.log('nodes', nodes);
+
+
+  var svg = d3.select(svgContainer.value).append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  svg.selectAll(".link")
+    .data(nodes.links())
     .enter().append("path")
-      .attr("class", "link")
-	  .attr("fill","none")
-	  .attr("stroke","#ccc")
+    .attr("class", "link")
+    .attr("fill", "none")
+    .attr("stroke", "#ccc")
     .attr("stroke-width", 2)
-      .attr("d", d3.linkRadial()
-          .x(function(d) { return d.y; })
-          .y(function(d) { return d.x; }));
+    .attr("d", diagonal)
 
-  const node = svgElement.selectAll('.node')
+  var node = svg.selectAll(".node")
     .data(root.descendants())
     .enter()
-    .append('g')
+    .append("g")
     .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
-    .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+    .attr("transform", function(d) { return "translate(" + (d.y) + "," + d.x + ")"; });
 
-  node.append('circle')
-    .attr('r', 2.5)
-    .attr('stroke', 'indianred')
-    .attr('stroke-width', 2)
-    .attr('fill', 'white');
+
+  node.append("circle")
+    .attr("r", 2.5)
+    .attr("stroke", "indianred")
+    .attr("stroke-width", 2)
+    .attr("fill", "white");
+
+  
 }
 
 async function fetchData() {
